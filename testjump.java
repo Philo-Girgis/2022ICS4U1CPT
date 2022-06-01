0cd704f262b591b14e98845810f37d4c821d41b0
Philo-Girgis <106279530+Philo-Girgis@users.noreply.github.com>
2022-05-31 20:04:02 -0400

    update to jump test
    creating the screen to view the jump which has yet to be created

--- testjump.java
+++ testjump.java
@@ -7,6 +7,7 @@ public class testjump implements ActionListener{
 	//properties
 	testlogic tlmodel;
 	tjpanel thepanel = new tjpanel();
+	JFrame theframe = new JFrame("test jump");
 	
 	//methods
 	public void actionPerformed(ActionEvent evt){
@@ -14,6 +15,13 @@ public class testjump implements ActionListener{
 	
 	//constructor
 	public testjump(){
+		thepanel.setPreferredSize(new Dimension(800, 600));
+		
+		theframe.setContentPane(thepanel);
+		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theframe.pack();
+		theframe.setResizable(false);
+		theframe.setVisible(true);
 	}
 	
 	//main method
--- testlogic.java
+++ testlogic.java
@@ -1,5 +1,8 @@
+//this is where I can do calculations whenever a jump is made
+
 public class testlogic{
 	//properties
+	
 
 
 	//methods
