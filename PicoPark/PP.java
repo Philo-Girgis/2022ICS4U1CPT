d9adee06f471508f83dbf5e4311ae9ea0da40895
Philo-Girgis <106279530+Philo-Girgis@users.noreply.github.com>
2022-06-03 11:30:33 -0400

    small edits
    in preparation for javadoc creation

--- PicoPark/PP.java
+++ PicoPark/PP.java
@@ -1,3 +1,10 @@
+//Application: Pico Park
+//Creators: Philo, Jerry, Alan
+//Last Edited: 6/2/2022
+//Version: 0.001
+
+//i'm pretty sure this is the only program that requires a javadoc
+
 public class PP{
 	//properties
 	
--- PicoPark/PicoPark.java
+++ PicoPark/PicoPark.java
@@ -3,11 +3,6 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-//Application: Pico Park
-//Creators: Philo, Jerry, Alan
-//Last Edited: 6/2/2022
-//Version: 0.001
-
 public class PicoPark implements ActionListener{
 	//properties
 	
