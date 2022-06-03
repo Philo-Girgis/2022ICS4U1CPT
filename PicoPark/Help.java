40543149e6779a5489899e66a9c0c49d6c36c39d
Philo-Girgis <106279530+Philo-Girgis@users.noreply.github.com>
2022-06-03 10:37:44 -0400

    additional bases
    the base file for a help menu and the csv's for levels have been added

---
+++ PicoPark/Help.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class Help extends JPanel{
+	//properties
+	
+	//methods
+	public void paintComponent(Graphics g){
+		super.paintComponent(g);
+	}
+	
+	//constructor
+	public Help(){
+	}
+	
+}
