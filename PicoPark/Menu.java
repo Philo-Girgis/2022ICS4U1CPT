64b9e5bd259e6b3fd1ea4c617066b3bb400c3097
Philo-Girgis <106279530+Philo-Girgis@users.noreply.github.com>
2022-06-04 22:59:49 -0400

    menu update
    there is a background animation and Jitems have been created. haven't added the items yet though.

--- PicoPark/Menu.java
+++ PicoPark/Menu.java
@@ -1,13 +1,55 @@
 import java.awt.*;
 import javax.swing.*;
 
+//am gonna try to set up a background animation
 
 public class Menu extends JPanel{
 	//properties
+	int intP1X = -250;
+	int intP1Y = 480;
+	int intP2X = -200;
+	int intP2Y = 480;
+	int intP3X = -150;
+	int intP3Y = 480;
+	int intP4X = -100;
+	int intP4Y = 480;
+	int intP5X = -50;
+	int intP5Y = 480;
+	int intXchange = 5;
 	
 	//methods
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
+		g.setColor(Color.WHITE);
+		g.fillRect(0,0,1280,720);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 500, 1500,400);
+		g.setColor(Color.RED);
+		g.fillRect(intP1X,intP1Y,20,20);
+		g.setColor(Color.GREEN);
+		g.fillRect(intP2X,intP2Y,20,20);
+		g.setColor(Color.YELLOW);
+		g.fillRect(intP3X,intP3Y,20,20);
+		g.setColor(Color.BLUE);
+		g.fillRect(intP4X,intP4Y,20,20);
+		g.setColor(Color.ORANGE);
+		g.fillRect(intP5X,intP5Y,20,20);
+		intP1X = intP1X + intXchange;
+		intP2X = intP2X + intXchange;
+		intP3X = intP3X + intXchange;
+		intP4X = intP4X + intXchange;
+		intP5X = intP5X + intXchange;
+		if(intP1X > 1500){
+			intP1X = -100;
+		}else if(intP2X > 1500){
+			intP2X = -100;
+		}else if(intP3X > 1500){
+			intP3X = -100;
+		}else if(intP4X > 1500){
+			intP4X = -100;
+		}else if (intP5X > 1500){
+			intP5X = -100;
+		}
 	}
 	
 	//constructor
--- PicoPark/PicoPark.java
+++ PicoPark/PicoPark.java
@@ -3,16 +3,71 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 
-public class PicoPark implements ActionListener{
+public class PicoPark implements ActionListener, KeyListener{
 	//properties
+	JFrame theframe = new JFrame("Pico Park");
+	Menu menupanel = new Menu();
+	Lobby lobbypanel = new Lobby();
+	Help helppanel = new Help();
+	Levels levelspanel = new Levels();
+	Timer thetimer = new Timer(1000/60, this);
+	JButton joinbutton = new JButton("Join");
+	JButton hostbutton = new JButton("Host");
+	JButton helpbutton = new JButton("Help");
+	//Should the port number be a set value?
+	//that way players just have to enter the ip address of who they want to join
+	//i'm gonna go with that for now cause it makes things simpler
+	//which means we gotta pick a constant port number now
+	JTextField joinfield = new JTextField();
+	JLabel faillabel = new JLabel("");
+	//faillabel.setFont(new Font) should allow me to change the font
 	
 	//methods
 	public void actionPerformed(ActionEvent evt){
+		if(evt.getSource() == thetimer){
+			menupanel.repaint();
+			lobbypanel.repaint();
+			helppanel.repaint();
+			levelspanel.repaint();
+		}
+	}
 	
+	public void keyPressed(KeyEvent evt){
+		
+	}
+	
+	public void keyReleased(KeyEvent evt){
+		
+	}
+	
+	public void keyTyped(KeyEvent evt){
+		
 	}
 	
 	//constructor
 	public PicoPark(){
+		//panels
+		menupanel.setPreferredSize(new Dimension(1280, 720));
+		menupanel.setLayout(null);
+		lobbypanel.setPreferredSize(new Dimension(1280, 720));
+		lobbypanel.setLayout(null);
+		helppanel.setPreferredSize(new Dimension(1280, 720));
+		helppanel.setLayout(null);
+		levelspanel.setPreferredSize(new Dimension(1280, 720));
+		levelspanel.setLayout(null);
+		
+		//add buttons to panels
+		//add action listeners to buttons
+		
+		//frame
+		theframe.setContentPane(menupanel);
+		theframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		theframe.pack();
+		theframe.setResizable(false);
+		theframe.setVisible(true);
+		theframe.addKeyListener(this);
+		thetimer.start();
+		
 	}
 	
 	//main method
